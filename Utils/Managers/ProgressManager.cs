58410a3ad7baaed27b6b6041bcfcd58d04c03979
Nekai <alessandrobelotti4570@gmail.com>
2022-06-11 23:36:10 +0200

    Create ProgressManager class for future implementation.

---
+++ Utils/Managers/ProgressManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyDexDB.Utils.Managers {
+	public class ProgressManager : EntityManager<Progress, ulong> {
+		
+	}
+}
